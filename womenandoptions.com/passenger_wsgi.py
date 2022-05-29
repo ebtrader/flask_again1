cb73bd8c59359883f4943f6f265a54f82fe4400a
ebtrader <jsiddique@gmail.com>
2022-05-29 11:06:43 -0400

    commit

--- womenandoptions.com/passenger_wsgi.py
+++ womenandoptions.com/passenger_wsgi.py
@@ -1,6 +1,6 @@
 import sys, os
 
-INTERP = os.path.join(os.environ['HOME'], 'womenandoptions.com', 'venv', 'bin', 'python3')
+INTERP = os.path.join(os.environ['HOME'], '', 'venv', 'bin', 'python3')
 if sys.executable != INTERP:
     os.execl(INTERP, INTERP, *sys.argv)
 sys.path.append(os.getcwd())
