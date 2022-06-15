65b1e2dba45777d35c0fd21a3b36106e56bf24ae
ebtrader <jsiddique@gmail.com>
2022-06-15 08:53:16 -0400

    Commit

--- yassirtrading.shop/old/06.15.22_passenger_wsgi.py
+++ yassirtrading.shop/old/06.15.22_passenger_wsgi.py
@@ -133,33 +133,3 @@ def graph():
 
         return render_template('neckline.html', graphJSON=graphJSON)
     return render_template('dropdown.html')
-
-@application.route('/calculator', methods=['GET', 'POST'])
-def target():
-    if request.method == "POST":
-        details = request.form
-
-        target = details['ticker']
-        target = int(target)
-        tqqq_ticker = "TQQQ"
-
-        df_tqqq = yf.download(tickers=tqqq_ticker, interval='5m', period ='1d')
-        recent_tqqq = df_tqqq['Close'].iloc[-1]
-        # print(df_tqqq['Close'])
-        # print(recent_tqqq)
-
-        nq_ticker = 'NQ=F'
-        df_nq = yf.download(tickers=nq_ticker, interval='5m', period ='1d')
-        recent_nq = df_nq['Close'].iloc[-1]
-        # print(df_nq['Close'])
-        # print(recent_nq)
-
-
-        nq_current = recent_nq
-        tqqq_current = recent_tqqq
-        nq_target = target
-        tqqq_target = ((nq_target / nq_current - 1) * 3 + 1)* tqqq_current
-        # print(tqqq_target)
-        tqqq_display = str(tqqq_target)
-        return tqqq_display
-    return render_template('target.html')
